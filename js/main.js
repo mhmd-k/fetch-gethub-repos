6c0801a050abdf247fa0fc0c459366cde916261a
mhmd-k <mhmdk425@gmail.com>
2022-09-27 17:31:04 +0300

    changed the design

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const input = document.querySelector(".form input");
 const getReposBtn = document.querySelector(".form button#get-repos");
-const fieldEmpty = document.querySelector(".form span");
 
 getReposBtn.addEventListener("click", () => {
   getRepos(input.value);
@@ -10,13 +9,12 @@ input.addEventListener("keydown", function (e) {
     getRepos(input.value);
   }
 });
-
+let s = 0;
+let f = 0;
 function getRepos(username) {
   if (input.value === "" || input.value === null) {
-    fieldEmpty.classList.add("empty");
-    fieldEmpty.innerHTML = `username is empty`;
+    console.log("empty");
   } else {
-    document.querySelector(".form").style.display = "none";
     fetch(`https://api.github.com/users/${username}/repos`)
       .then((respons) => {
         return respons.json();
@@ -24,27 +22,23 @@ function getRepos(username) {
       .then((r) => {
         console.log(r);
         if (r.length === 0) {
-          fieldEmpty.classList.add("empty");
-          fieldEmpty.innerHTML = "username not valid";
+          console.log("empty");
           return;
         } else {
-          if (r.length > 15) {
-            r.length = 15;
-          }
-          document.querySelector(
-            ".container h2"
-          ).innerHTML = `${input.value} Repositories`;
+          // remove form and display container
+          document.querySelector(".form").style.display = "none";
+          document.querySelector(".container").classList.remove("none");
+          // end remove form and display container
           document.querySelector(".repos").innerHTML = "";
           input.value = "";
-          fieldEmpty.classList.remove("empty");
           for (let i in r) {
+            s += r[i].stargazers_count;
+            f += r[i].forks;
             let box = document.createElement("div");
             box.classList.add("box");
             let repoName = document.createElement("h3");
             repoName.innerText = r[i].name;
             let ul = document.createElement("ul");
-            let id = document.createElement("li");
-            id.innerText = `id: ${r[i].id}`;
             let visibility = document.createElement("li");
             visibility.innerText = `visibility: ${r[i].visibility}`;
             let watchers = document.createElement("li");
@@ -59,10 +53,51 @@ function getRepos(username) {
             let url = document.createElement("li");
             url.innerHTML = `link: <a href="${r[i].svn_url}">${r[i].svn_url}</a>`;
             document.querySelector(".repos").append(box);
-            ul.append(id, visibility, watchers, language, forks, url);
+            ul.append(visibility, watchers, language, forks, url);
             box.append(repoName, ul);
           }
+          // profile
+          document
+            .querySelector(".container .profile img")
+            .setAttribute("src", `${r[0].owner.avatar_url}`);
+          console.log(r[0].owner.url);
+          fetch(`https://api.github.com/users/${username}`)
+            .then((url) => {
+              return url.json();
+            })
+            .then((url) => {
+              document.querySelector(".container .profile h2.name").innerHTML =
+                url.name;
+              document.querySelector(
+                ".container .profile .info .followers .num"
+              ).innerText = url.followers;
+              document.querySelector(
+                ".container .profile .info .following .num"
+              ).innerText = url.following;
+            });
+          document.querySelector(
+            ".container .profile h2 a"
+          ).innerHTML = `@${username}`;
+          document
+            .querySelector(".container .profile h2 a")
+            .setAttribute("href", r[0].owner.html_url);
+          document.querySelector(
+            ".container .profile .icon .stars"
+          ).innerHTML = `<i class="fa-solid fa-star"></i> ${s}`;
+          document.querySelector(
+            ".container .profile .icon .forks"
+          ).innerHTML = `<i class="fa-solid fa-code-fork"></i> ${f}`;
+          document.querySelector(
+            ".container .profile .info .repositories .num"
+          ).innerText = r.length;
+          // end profile
         }
       });
   }
 }
+
+// back button
+const backBtn = document.querySelector(".refresh");
+backBtn.addEventListener("click", () => {
+  window.location.reload();
+});
